6ef56565556987e701fed797a405f0825fe2e15a
Todd DeLuca <nospam@todddeluca.com>
2013-03-05 12:14:57 -0500

    Bump version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'dones',
-    version = '0.1.0',
+    version = '0.2.0',
     license = 'MIT',
     description = 'Track what keys are "done" using MySQL to implement a '
                   'simple key-store.',
